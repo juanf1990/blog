dd83818da49897faae912b20050b2ee7cb5e71d8
Juan Ferreira <juanandres15@gmail.com>
2023-02-13 11:17:24 +0000

    change to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,13 @@
 "use client";
-import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import Router from "next/router";
 
 function Footer() {
-  const [toggle, setToggle] = useState(false);
-  const router = Router;
-
   return (
     <footer className="flex items-center justify-end font-bold p-2 fixed bg-slate-500 w-full rounded-t z-50 opacity-90 bottom-0">
       <div>
         <button className="px-4 py-1 md:text-base bg-gray-900 text-orange-500 flex items-center rounded-full text-center text-xs">
-          <Link
-            onClick={() => setToggle(!toggle)}
-            href={toggle ? "/api/exit-preview" : "/api/preview"}
-          >
-            Preview Mode
+          <Link href={"http://www.github.com/juanf1990"}>
+            Made by Juan Ferreira
           </Link>
         </button>
       </div>
