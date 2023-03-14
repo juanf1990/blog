dc59ccf608002ea1fe07517a14324d1d8adaf1fc
Juan Ferreira <juanandres15@gmail.com>
2023-03-14 11:28:25 +0000

    feat: removed studionavbar for debugging

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,26 +1,28 @@
-import {defineConfig, StudioLogo} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
-import {myTheme} from './theme'
-import StudioNavbar from './components/StudioNavbar'
-import Logo from './components/Logo'
-import { getDefaultDocumentNode } from './structure'
+import { defineConfig, StudioLogo } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+import { myTheme } from "./theme";
+// import StudioNavbar from './components/StudioNavbar'
+import Logo from "./components/Logo";
+import { getDefaultDocumentNode } from "./structure";
 
-const projectId =  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID !
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET !
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
-  basePath: '/studio',
-  name: 'BLOG',
-  title: 'Personal blog',
-  
+  basePath: "/studio",
+  name: "BLOG",
+  title: "Personal blog",
+
   projectId,
   dataset,
-  plugins: [deskTool({
-    defaultDocumentNode: getDefaultDocumentNode
-  }),
-  visionTool()],
+  plugins: [
+    deskTool({
+      defaultDocumentNode: getDefaultDocumentNode,
+    }),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
@@ -28,8 +30,8 @@ export default defineConfig({
   studio: {
     components: {
       logo: Logo,
-      navbar: StudioNavbar,
+      // navbar: StudioNavbar,
     },
   },
   theme: myTheme,
-})
+});
