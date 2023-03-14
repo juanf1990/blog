08c2ef2b7fa141448ac97b7cac72cada936a071e
Juan Ferreira <juanandres15@gmail.com>
2023-03-14 23:24:36 +0000

    feat: added animations to header and current date

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,21 +9,30 @@ function Header() {
   const router = Router;
 
   return (
-    <header className="font-bold px-10 py-2 fixed bg-slate-500 w-full rounded-b z-50 opacity-90 max-w-7xl">
-      <div className="flex flex-row-reverse items-center justify-between">
+    <header className="font-bold px-10 py-2 fixed bg-slate-500 w-full rounded-b z-50 opacity-90 max-w-7xl flex justify-between">
+      <div className="flex flex-row-reverse items-center justify-end gap-x-5">
+        <span className="animate-pulse">
+          {new Date().toLocaleDateString("en-US", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          })}
+        </span>
         <Link href="/">
-          <Image
-            className="rounded-full"
-            src="http://placeimg.com/460/460/people"
-            width={50}
-            height={20}
-            alt="logo"
-          />
-        </Link>
-        <Link href="/">
-          <h1 className="text-[#E3E3E0]">The Blog</h1>
+          <h1 className="text-[#E3E3E0] hover:scale-110 transition-all duration-300 ease-in-out animate-pulse">
+            Blog
+          </h1>
         </Link>
       </div>
+      <Link href="/">
+        <Image
+          className="rounded-full"
+          src="http://placeimg.com/460/460/people"
+          width={50}
+          height={20}
+          alt="logo"
+        />
+      </Link>
     </header>
   );
 }
